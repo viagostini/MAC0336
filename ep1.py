4fc73e11d59d5786686b43185540abe7a8ba15b7
Vinicius Agostini <vinicius.agostini19@gmail.com>
2020-05-18 17:42:38 -0300

    update

--- ep1.py
+++ ep1.py
@@ -1,18 +1,13 @@
 # coding: utf-8
 
 """
-    Esqueleto para o EP1 de Cripto 2020
+    EP1 de Cripto 2020
     Prof. Routo Terada
     Monitor: Thales Paiva
 
-    Exemplo de como rodar o arquivo e saída do programa:
-    [~/cripto-2020/ep1]$ sage ep1_esqueleto.py documento.txt
-    O Hash SHA512 do arquivo documento.txt é
-    dd8996d0f9f8c17056ab3314105ca4043ef729adf8b0f5f84e0d71ef8e51765cfa58741df01aaa5ae5642c54ecf47b2de02140f95956d3d6370e2ade18ef7a13
-    O inteiro correspondente ao hash para uso no ElGamal é
-    11602858124117136456103271775114122754696733789413816382276456553852239770973923409586227117425499556575579222816159862027314152111415107121278401054210579
-    A distância de Hamming entre 0x4123 e 0x3189 é
-    7
+    Autor: Vinicius Agostini
+
+    Feito a partir do esqueleto fornecido no PACA pelo monitor.
 """
 
 from sage.all import *
@@ -23,16 +18,13 @@ import sys
 import string
 import random
 import hashlib
-from binascii import hexlify, unhexlify
-
-# para texto em negrito
-class color:
-    BOLD = '\033[1m'
-    END = '\033[0m'
+from binascii import hexlify
 
 # Parâmetros públicos p e g
-# PRIME = 3329499024484696430955445194464218832905973351121497617435753366182222251575714808510036328892050841
-# GENERATOR = 17
+
+# Na primeira parte do EP, para as assinaturas com ElGamal, Alice e Beto
+# usam primos e geradores diferentes. Na segunda parte, para o KEM, são
+# utilizados os mesmos primos e geradores.
 PRIME_A = 10556903207286767181741614405391493132158714643702501887676502954427109116849995896933852122361081109139592877832545731729513028521947294977454763046753869
 GENERATOR_A = 2
 
@@ -51,6 +43,9 @@ def elgamal_keygen(prime, generator):
 def random_coprime(n):
     '''
     Devolve um número x coprimo em relação a n tal que gcd(x, n) = 1.
+
+    A probabilidade de encontrar um número que satisfaça isso é grande
+    o suficiente para usar um método probabilístico.
     '''
     x = 0
 
@@ -109,18 +104,12 @@ def KEM_get_key(r, u, prime, secret_key, public_key):
 
     return K
 
-def get_binary_representation_512_bits_of_hex(h):
-    '''
-    Recebe uma string h representando um hexadecimal.
-    Devolve a representação binária de h com EXATAMENTE 512 bits
-    '''
-
-    bin_h = bin(int(h, 16))[2:]  # [2:] Tira o 0b da frente
-    bin_h_512bits = '0' * (512 - len(bin_h)) + bin_h
-
-    return bin_h_512bits
-
 def hamming_int(a, b):
+    '''
+    A distância de hamming é o número de bits 1 em a xor b.
+
+    Note que a & (a - 1) desliga o último bit 1 de a.
+    '''
     count = 0
     xor = int(a)^int(b)
     
@@ -128,25 +117,6 @@ def hamming_int(a, b):
         count += 1
         xor &= (xor - 1)
     return count
-
-def hamming_distance_with_hex_strings(h1, h2):
-    """
-    Calcula a distância de hamming entre dois hashes em STRINGS representando
-    hexadecimais
-    """
-
-    #bin_h1 = get_binary_representation_512_bits_of_hex(h1)
-    #bin_h2 = get_binary_representation_512_bits_of_hex(h2)
-
-    bin_h1 = ascii_to_bin(h1)
-    bin_h2 = ascii_to_bin(h2)
-
-    distance = 0
-    for x1, x2 in zip(bin_h1, bin_h2):
-        if x1 != x2:
-            distance += 1
-    return distance
-
 
 def sha512_file(filepath):
     BLOCK_SIZE = 65536  # = 64Kb
@@ -166,15 +136,25 @@ def read_file(filepath):
     file.close()
     return text
 
-def modify_file(file):
-    new_file = list(file)
+def modify_text(text):
+    '''
+    Altera o caractere de uma posição aleatória no texto original
+    por uma letra ascii aleatória. O loop serve para que caso a
+    letra escolhida seja a mesma, tentamos novamente.
+    Já que para este EP queremos que os textos sejam de fato diferentes,
+    vale a pena fazer isso para garantir, já que não existem tantas letras.
+    '''
+    new_text = list(text)
     
-    idx = randint(0, len(new_file))
-    new_file[idx] = random.choice(string.ascii_letters)
+    idx = randint(0, len(new_text))
     
-    return str(new_file)
+    while new_text[idx] == text[idx]:
+        new_text[idx] = random.choice(string.ascii_letters)
+    
+    return str(new_text)
 
 def to_hex(*args):
+    # returns tuple of sha512 hashes for every argument
     return (hashlib.sha512(str(key)).hexdigest() for key in args)
 
 def main():
@@ -182,23 +162,6 @@ def main():
     Função principal que ordena as chamadas de funções para realizar o que foi
     pedido no EP.
     """
-    if len(sys.argv) != 2:
-        print('Uso: %s <documento>' % sys.argv[0])
-        sys.exit(1)
-
-    # Abaixo mostro como calcular o hash de um arquivo:
-    # filepath = sys.argv[1]
-    # filehash = sha512_file(filepath)
-    
-    # print('O Hash SHA512 do arquivo %s é' % (filepath))
-    # print(filehash)
-
-    # print('O inteiro correspondente ao hash para uso no ElGamal é')
-    # print(int(filehash, 16))
-
-    # print('A distância de Hamming entre 0x4123 e 0x3189 é')
-    # print(hamming_distance_with_hex_strings('0x4123', '0x3189'))
-
 
     # 1.
     title = ' Item 1 - Geração de chaves de Alice e Beto '
@@ -229,26 +192,40 @@ def main():
         "public_key: {}\n\n"
     .format(*to_hex(pB, gB, SB, TB)))
    
+
+
+    # -----------------------------------------------------------
+
+   
     # 2
     title = ' Item 2 - Primeiros 100 caracteres de documento1 (em hexadecimal) '
     print('\n' + title.center(100, '-') + '\n')
+    
     nusp = 4367487
     num_bytes = int(1e5)
     random_bytes_hex = hexlify(os.urandom(num_bytes))
 
-    #print('\n Primeiros 100 caracteres de documento1 (em hexadecimal):')
     documento1 = str(nusp) + random_bytes_hex
     print(documento1[:100] + '\n\n')
 
 
+
+    # -----------------------------------------------------------
+
+   
     # 3
     title = ' Item 3 - Primeiros 100 caracteres de documento2 (em hexadecimal) '
     print('\n' + title.center(100, '-') + '\n')
 
-    #print('\n\n Primeiros 100 caracteres de documento2 (em hexadecimal):')
+    
     documento2 = str(nusp + 1) + random_bytes_hex
     print(documento2[:100] + '\n\n')
 
+
+
+    # -----------------------------------------------------------
+
+   
     # 4
     title = ' Item 4 - Hash de documento1 e documento2 '
     print('\n' + title.center(100, '-') + '\n')
@@ -261,6 +238,11 @@ def main():
     hash2 = hashlib.sha512(documento2).hexdigest()
     print(hash2 + '\n\n')
     
+
+
+    # -----------------------------------------------------------
+
+   
     # 5
     title = ' Item 5 - Assinatura de hash1 e hash2 '
     print('\n' + title.center(100, '-') + '\n')
@@ -270,10 +252,16 @@ def main():
     print("y: {}\n"  "z: {}\n".format(*to_hex(*assinaturaHash1)))
 
     print('\n Assinatura de hash2')
-    assinaturaHash2 = elgamal_sign(hash1, pA, gA, SA)
+    assinaturaHash2 = elgamal_sign(hash2, pA, gA, SA)
     print("y: {}\n"  "z: {}\n".format(*to_hex(*assinaturaHash2)))
 
+
+
+    # -----------------------------------------------------------
+
+   
     # 6
+    # como esperado: a distancia é de aproximadamente metade dos bits
     title = ' Item 6 - Distância de Hamming de assinaturaHash1 e assinaturaHash2 '
     print('\n' + title.center(100, '-') + '\n')
     
@@ -283,6 +271,11 @@ def main():
     hamming_z = hamming_int(z1, z2)
     print(str(hamming_y + hamming_z) + '\n')
 
+
+
+    # -----------------------------------------------------------
+
+   
     # 7
     title = ' Item 7 - Verificação de assinaturaHash1 sobre hash1 '
     print('\n' + title.center(100, '-') + '\n')
@@ -294,6 +287,11 @@ def main():
         "lado direito: {}\n\n"
     .format(verify[0], *to_hex(*verify[1:])))
 
+
+
+    # -----------------------------------------------------------
+
+   
     # 8
     title = ' Item 8 - Verificação de assinaturaHash1 sobre hash2 '
     print('\n' + title.center(100, '-') + '\n')
@@ -305,6 +303,11 @@ def main():
         "lado direito: {}\n\n"
     .format(verify[0], *to_hex(*verify[1:])))
 
+
+
+    # -----------------------------------------------------------
+
+   
     # 9.1
     title = ' Item 9.1 - Hash do Arquivo arq1.txt (hashA) '
     print('\n' + title.center(100, '-') + '\n')
@@ -312,14 +315,24 @@ def main():
     hashA = sha512_file('arq1.txt')
     print(hashA + '\n\n')
     
+
+
+    # -----------------------------------------------------------
+
+   
     # 9.2
     title = ' Item 9.2 - Assinatura de hashA '
     print('\n' + title.center(100, '-') + '\n')
     assinaturaHashA = elgamal_sign(hashA, pA, gA, SA)
     print("y: {}\n\nz: {}\n\n".format(*to_hex(*assinaturaHashA)))
     
+
+
+    # -----------------------------------------------------------
+
+   
     # 9.3
-    title = ' Item 9.3 - Verificação de assinaturaHAshA sobre hashA '
+    title = ' Item 9.3 - Verificação de assinaturaHashA sobre hashA '
     print('\n' + title.center(100, '-') + '\n')
 
     verify = elgamal_verify(assinaturaHashA, hashA, pA, gA, TA)
@@ -329,13 +342,20 @@ def main():
         "lado direito: {}\n\n"
     .format(verify[0], *to_hex(*verify[1:])))
 
-    # 9.4
-    # ---------- Item 9.4 ----------
-    arq1 = read_file('arq1.txt')
-    arq1mod = modify_file(arq1)
 
+    # -----------------------------------------------------------
+
+   
+    # 9.4 (Modificação de arq1.txt em posição aleatoria)
+    arq1 = read_file('arq1.txt')
+    arq1mod = modify_text(arq1)
+
+
+    # -----------------------------------------------------------
+
+   
     # 9.5
-    title = ' Item 9.5 - Criação e assinatura de arq1.txt modificado '
+    title = ' Item 9.5 - Hash e assinatura de arq1.txt modificado '
     print('\n' + title.center(100, '-') + '\n')
 
     print(' Hash de arq1.txt modificado')
@@ -347,7 +367,11 @@ def main():
     print("y: {}\n\nz: {}\n\n".format(*to_hex(*assinaturaHashAmod)))
 
 
+    # -----------------------------------------------------------
+
+   
     # 9.6
+    # como esperado: a distancia é de aproximadamente metade dos bits
     title = ' Item 9.6 - Distancia de Hamming de assinaturaHash1 e assinaturaHash2 '
     print('\n' + title.center(100, '-') + '\n')
 
@@ -357,11 +381,17 @@ def main():
     hamming_z = hamming_int(z1, z2)
     print(str(hamming_y + hamming_z) + '\n')
 
-    # 10.a
-    #print('\n\n\n\n# ---------- Item 10.1 ---------- #')
-    arq2 = read_file('arq1.txt') + str(y1) + str(z1)
-    #print(arq2)
 
+    # -----------------------------------------------------------
+
+   
+    # 10.a
+    arq2 = read_file('arq1.txt') + str(y1) + str(z1)
+
+
+    # -----------------------------------------------------------
+
+   
     # 10.b
     title = ' Item 10.2 - Hash de arq2 <- arq1 | assinaturaHashA '
     print('\n' + title.center(100, '-') + '\n')
@@ -369,14 +399,21 @@ def main():
     hash2 = hashlib.sha512(arq2).hexdigest()
     print(hash2 + '\n\n')
 
+
+    # -----------------------------------------------------------
+
+   
     # 10.c
     title = ' Item 10.3 - Assinatura de Beto sobre arq2 '
     print('\n' + title.center(100, '-') + '\n')
 
     hash1 = hashlib.sha512(arq1).hexdigest()
-    #assinaturaHashA = elgamal_sign(hash1, pA, gA, SA)
     assinaturaHashB = elgamal_sign(hash2, pB, gB, SB)
     print("y: {}\n\nz: {}\n\n".format(*to_hex(*assinaturaHashB)))
+
+
+    # -----------------------------------------------------------
+
 
     # 10.d
     title = ' Item 10.4 - Verificação da assinatura de Beto sobre arq2 '
@@ -389,6 +426,11 @@ def main():
         "lado direito: {}\n\n"
     .format(verify[0], *to_hex(*verify[1:])))
     
+
+
+    # -----------------------------------------------------------
+
+   
     # 11
     title = ' Item 11 - Aplicação do algoritmo KEM '
     print('\n' + title.center(100, '-') + '\n')
@@ -401,9 +443,16 @@ def main():
     kb = KEM_get_key(rb, ua, pA, SB, TA)
     print("KA: {}\n\nKB: {}\n\n".format(*to_hex(ka, kb)))
 
+
+    # -----------------------------------------------------------
+
+    # como esperado: distância é 0 pois são iguais
     title = ' Item 12 - Distância de Hamming entre KA e KB '
     print('\n' + title.center(100, '-') + '\n')
     print(str(hamming_int(ka, kb)) + '\n')
+
+
+    # -----------------------------------------------------------
 
 if __name__ == '__main__':
     main()
